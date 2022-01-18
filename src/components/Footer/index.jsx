f0a03884e5f532c1105e5a2973cfd48c2a4c890e
Rafael Tivane <luislangabusiness@gmail.com>
2022-01-18 14:15:15 +0200

    finishing homepage footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -2,7 +2,7 @@ import FacebookIcon from '@mui/icons-material/Facebook';
 import InstagramIcon from '@mui/icons-material/Instagram';
 import TwitterIcon from '@mui/icons-material/Twitter';
 import classNames from 'classnames';
-import { useDisplay, useTypography } from '../../styles';
+import { useDisplay, useResponsive, useTypography } from '../../styles';
 import { useStyles } from './styles';
 import { Typography } from '@mui/material'
 import { Link } from 'react-router-dom'
@@ -10,12 +10,14 @@ import { Link } from 'react-router-dom'
 const Footer = ({ isHomePage }) => {
     const classes = useStyles();
     const display = useDisplay();
+    const responsive = useResponsive();
     const text = useTypography();
 
     return (
         <footer className={classNames(display.flex, display.flexColumn, display.justifyBetween, 
-            classes.footer, display.px, display.pb3, display.pt3, {[classes.footerHomePage]: isHomePage })}>
-            <div>
+            classes.footer, display.px, display.pb3, display.pt3, {[classes.footerHomePage]: isHomePage },
+            responsive.smRow, responsive.smAlignStart)}>
+            <div className={classNames(classes.footerContent)}>
                 <Typography 
                     variant="h4" 
                     component={Link} 
@@ -23,11 +25,12 @@ const Footer = ({ isHomePage }) => {
                     className={classNames({[text.textLight]: isHomePage}, text.noUnderline, classes.footerTitle)}>
                     Modern<br/>art gallery
                 </Typography>
-                <Typography className={classNames(text.rem9, display.mt1, display.opacity9, {[text.textLight]: isHomePage})}>
+                <Typography className={classNames(text.rem9, display.mt1, display.opacity9, 
+                    {[text.textLight]: isHomePage}, responsive.mdMl2, classes.footerDescription)}>
                     The Modern Art Gallery is free to all visitors and open seven days a week from 8am to 9pm. Find us at 99 King Street, Newport, USA.
                 </Typography>
             </div>
-            <ul className={classNames(display.flex, display.alignCenter, display.mt2)}>
+            <ul className={classNames(display.flex, display.alignCenter, display.mt2, responsive.smMt0)}>
                 <li className={classNames(display.mr1)}>
                     <FacebookIcon className={classNames({[text.textLight]: isHomePage})} />
                 </li>
--- src/components/Footer/styles.js
+++ src/components/Footer/styles.js
@@ -6,7 +6,20 @@ export const useStyles = makeStyles(theme => ({
     footerHomePage: {
         backgroundColor: '#000'
     },
+    footerContent: {
+        [theme.breakpoints.up('md')]: {
+            display: 'flex'
+        }
+    },
     footerTitle: {
         fontWeight: '900 !important'
+    },
+    footerDescription: {
+        [theme.breakpoints.up('md')]: {
+            width: 296
+        },
+        [theme.breakpoints.up(1050)]: {
+            marginLeft: '4rem !important'
+        }
     }
 }));
